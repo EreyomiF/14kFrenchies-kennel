06e65c40e12ebd5bf88f8f9f91cd4a82dc8d690d
Ereyomi Funmilola <funmilolaereyomi@gmail.comgit config --global user.email funmilolaereyomi@gmail.com>
2024-03-10 00:19:17 +0100

    new

---
+++ src/Components/FooterSection.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-red-500 dark:bg-gray-900">
+      <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <div className="md:flex md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <a href="https://flowbite.com/" className="flex items-center">
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="h-8 me-3"
+                alt="FlowBite Logo"
+              />
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                Flowbite
+              </span>
+            </a>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            {/* ... Rest of your code ... */}
+          </div>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{' '}
+            <a href="https://flowbite.com/" className="hover:underline">
+              Flowbite™
+            </a>
+            . All Rights Reserved.
+          </span>
+          <div className="flex mt-4 sm:justify-center sm:mt-0">
+            {/* ... Social media icons ... */}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -17,14 +17,14 @@ const HeroSection = () => {
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
         <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right text-white">
           <h1 className="text-3xl font-extrabold sm:text-5xl">
-            Let us find your
-            <strong className="block font-extrabold text-rose-700"> Forever Home. </strong>
+          Welcome to       <br />
+         "14k Bullies & French Dogs"
+<strong className="block font-extrabold text-rose-700"> Alabama's best frenchies
+ </strong>
           </h1>
 
           <p className="mt-4 max-w-lg sm:text-xl/relaxed">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
-            numquam ea!
-          </p>
+          Join us on a journey where every bully is a testament to our dedication, and every home welcomes an unparalleled companion.           </p>
 
           <div className="mt-8 flex flex-wrap gap-4 text-center">
             <a
