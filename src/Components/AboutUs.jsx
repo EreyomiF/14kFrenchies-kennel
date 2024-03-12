73b699d06029d535144d7f91073761f899e4d817
Ereyomi Funmilola <funmilolaereyomi@gmail.comgit config --global user.email funmilolaereyomi@gmail.com>
2024-03-12 07:10:29 +0100

    j

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -34,7 +34,7 @@ incredible animals.
           </div>
 
           <div className="mb-12 lg:mb-0">
-            <div className="block h-full rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+            <div className="block h-full rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] ">
               <div className="flex justify-center">
                 <div className="-mt-8 inline-block rounded-full bg-primary-100 p-4 text-primary shadow-md">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2"
@@ -53,7 +53,7 @@ incredible animals.
           </div>
 
           <div className="">
-            <div className="block h-full rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+            <div className="block h-full rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] ">
               <div className="flex justify-center">
                 <div className="-mt-8 inline-block rounded-full bg-primary-100 p-4 text-primary shadow-md">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2"
